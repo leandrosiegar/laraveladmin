647ed7e568340dd9c540e98e021f4d3a50606360
unknown <leandrosiegar@gmail.com>
2020-11-10 17:13:56 +0100

    creando todo el adminlte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@endsection
+
+@section('content')
+  lo que sea
+@endsection
+
+@section('css')
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+@endsection
+
+@section('js')
+  <script>console.log("aqui iran los js");</script>
+@endsection
